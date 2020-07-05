dfe9e3eea63d358c2e40d019e7659dcd00228eac
S Sohail <ssohail@spartaglobal.com>
2020-07-05 22:53:28 +0100

    inheritance

--- mammal.py
+++ mammal.py
@@ -1,24 +1,30 @@
 from animal import Animal
 
 class Mammal(Animal):
-    def __init__(self, alive, hungry = True, sleeping = False, breathing = True, animaltype, prey, predator, origin):
-        super().__init__(alive, hungry = True, sleeping = False, breathing = True)
+     def __init__(self, alive, hungry, sleeping, breathing, animaltype, prey, predator, origin):
+        super().__init__(alive, hungry, sleeping, breathing) #This is importing the parent class into the child class
         self.animaltype = animaltype #Self is related to the class within init..
         self.prey = prey
         self.predator = predator
         self.origin = origin
 
-    def ground_sea_animal(self):
-        print("This type of animal is a ground sea animal, a shark")
+     def animaltype(self):
+        self.animaltype = "what the type of animal it is, in this case a Mammal."
+        print(self.animaltype)
 
-    def prey(self):
-        self.prey = "The prey of this animal, is a small fish"
-        print(self.prey)
+     def prey(self):
+         self.prey = "a prey is something the animal would like to eat"
+         print(self.prey)
 
-    def predator(self):
-        print("The predator of this animal is a larger shark")
+     def predator(self):
+         self.predator = "a predator is a animal that will hunt this animal"
+         print(self.predator)
 
-    def origin(self):
-        print("This is originated from North Sea")
+     def origin(self):
+         self.origin = "Where the animal is from globally"
+         print(self.origin)
 
+
+Tiger = Mammal(True, True, False, True, "Big cats", "Deer", "Crocodile", "Siberia")
+Tiger.animaltype()
 
